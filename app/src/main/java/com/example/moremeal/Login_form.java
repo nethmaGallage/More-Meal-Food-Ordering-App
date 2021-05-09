62f3d2c2b94927642d489d634b8f1048b5c3f833
nethmag <ssnethmagallage@gmail.com>
2021-05-09 15:02:03 +0530

    delete senverification method

--- app/src/main/java/com/example/moremeal/Login_form.java
+++ app/src/main/java/com/example/moremeal/Login_form.java
@@ -1,5 +1,7 @@
 package com.example.moremeal;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,16 +13,34 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class Login_form extends AppCompatActivity {
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login_form extends AppCompatActivity  {
 
     EditText username;
     EditText password;
@@ -28,6 +48,10 @@ public class Login_form extends AppCompatActivity {
     Button login;
 
 
+    String name = "Notification_channel";
+    String Channel_ID = "ID_1";
+    String description = "Sample Notification";
+    EditText editTextName;
 
 
 
@@ -40,7 +64,6 @@ public class Login_form extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_form);
 
-
         mAuth = FirebaseAuth.getInstance();
 
 
@@ -51,6 +74,37 @@ public class Login_form extends AppCompatActivity {
 
         setupUI();
         setupListeners();
+
+
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel(Channel_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+
+            //Creating the LayoutInflater instance
+            LayoutInflater li = getLayoutInflater();
+            //Getting the View object as defined in the custom toast.xml file
+            View layout = li.inflate(R.layout.customtoast, (ViewGroup)
+                    findViewById(R.id.custom_toast_layout));
+
+
+            //Creating the Toast object
+            Toast toast = new Toast(getApplicationContext());
+            toast.setDuration(Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            toast.setView(layout);
+            //setting the view of custom toast layout
+            toast.show();
+
+        }
 
 
 
@@ -73,9 +127,29 @@ public class Login_form extends AppCompatActivity {
                     reload();
 
                     //Intent create to redirect to Main-Activity 3
-                    Intent login = new Intent(Login_form.this, Success.class);
+                    Intent login = new Intent(Login_form.this,Success.class);
                     startActivity(login);
                 }
+
+
+                Intent intent = new Intent(getApplicationContext(),Signup_form.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), Channel_ID).setSmallIcon(R.drawable.ic_launcher_background).setContentTitle("My notification").setContentText("Hello World!").setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setContentIntent(pendingIntent).setAutoCancel(true);
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+                notificationManager.notify(0, builder.build());
+
+                Context context = getApplicationContext();
+                //The context to use. Usually your Application or Activity object
+                CharSequence message = "You just clicked the OK button";
+                //Display string  int
+                int duration = Toast.LENGTH_SHORT;
+                //How long the toast message will lasts
+                Toast toast = Toast.makeText(context, message, duration);
+                toast.show();
+                toast.setGravity(Gravity.TOP|Gravity.LEFT, 0, 0);
+
             }
         });
 
@@ -135,11 +209,7 @@ public class Login_form extends AppCompatActivity {
         if(isValid){
             String emailV = username.getText().toString();
             String passwordV = password.getText().toString();
-        if (emailV.equals("admin@admin.com") && passwordV.equals("admin1234")){
 
-            Intent toadmin = new Intent(Login_form.this,Success.class);
-            startActivity(toadmin);
-        }
             // [START sign_in_with_email]
             mAuth.signInWithEmailAndPassword(emailV, passwordV)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -156,7 +226,7 @@ public class Login_form extends AppCompatActivity {
                             } else {
                                 // If sign in fails, display a message to the user.
                                 Log.w(TAG, "signInWithEmail:failure", task.getException());
-                                Toast.makeText(Login_form.this, "Authentication failed",
+                                Toast.makeText(Login_form.this, "Authentication failed.",
                                         Toast.LENGTH_SHORT).show();
                                 updateUI(null);
                             }
@@ -168,7 +238,7 @@ public class Login_form extends AppCompatActivity {
 
         else{
 
-            Toast t = Toast.makeText(this,"Wrong email or password.Please try again " ,Toast.LENGTH_SHORT);
+            Toast t = Toast.makeText(this,"Wrong email or password " ,Toast.LENGTH_SHORT);
             t.show();
 
         }
